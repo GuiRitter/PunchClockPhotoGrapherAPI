56dc1dbb2c63b60c930ccb99037537564df5269e
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2023-04-11 10:56:29 -0300

    WIP fix

--- src/app/route/photoRoute.js
+++ src/app/route/photoRoute.js
@@ -2,10 +2,11 @@ import express from 'express';
 
 import verifyAuth from '../middleware/verifyAuth';
 
-import { get, list, put } from '../controller/photoController';
+import { compose, get, list, put } from '../controller/photoController';
 
 const router = express.Router();
 
+router.get('/compose', verifyAuth, compose);
 router.get('/get', verifyAuth, get);
 router.get('/list', verifyAuth, list);
 router.post('/put', verifyAuth, put);
