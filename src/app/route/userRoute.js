d7081cc62c54fa69cf8e3fb4d138a5d61231c8ef
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2024-04-24 22:42:21 -0300

    fixed user route

--- src/app/route/userRoute.js
+++ src/app/route/userRoute.js
@@ -2,10 +2,11 @@ import express from 'express';
 
 import { checkToken, signIn } from '../controller/userController';
 
+import verifyAuth from '../middleware/verifyAuth';
+
 const router = express.Router();
 
 router.get('/check', verifyAuth, checkToken);
 router.post('/sign_in', signIn);
 
 export default router;
-
